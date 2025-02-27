3a9bca1439648ddd85147f016f1a1ce9239c7f72
jwzzang12 <moonbb12@gmail.com>
2025-02-27 17:40:23 +0900

    Add JavaScript to home and menu elements; update hamburger layout

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('#main-menu');
+
+const menuItemTL = gsap.timeline().from('.menu-item', {
+  opacity: 0,
+  delay: 0.5,
+  stagger: { each: 0.1 },
+});
+
+const menuLogoTL = gsap.timeline().from('.menu-logo', {
+  opacity: 0,
+  delay: 0.5,
+  y: -50,
+  duration: 0.5,
+});
+
+menuBtn.addEventListener('click', function () {
+  menu.classList.toggle('open');
+  menuBtn.classList.toggle('open');
+  if (menu.classList.contains('open')) {
+    menuItemTL.restart();
+    menuLogoTL.restart();
+  }
+});
+
+menu.addEventListener('wheel', function (e) {
+  e.preventDefault();
+});
+
+gsap.to('.bg-cover', {
+  opacity: 0,
+  duration: 1,
+  y: -1000,
+  delay: 0.1,
+});
